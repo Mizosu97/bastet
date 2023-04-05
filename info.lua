cbead6f087d834ebf1bcfb7f85d8b1934d87182c
Mizosu97 <earthpig97@gmail.com>
2023-04-05 15:42:50 -0400

    packaging

---
+++ info.lua
@@ -0,0 +1,11 @@
+local info = {}
+
+info.pacman_depends = {"python", "cmatrix"}
+
+info.aur_depends = {}
+
+info.directories = {"$HOME/lol", "/etc/anotherlol"}
+
+info.files = {"/usr/bin/demoProgram"}
+
+return info
